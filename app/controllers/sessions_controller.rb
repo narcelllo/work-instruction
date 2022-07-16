e962018a7c8a1d93df4651a7ce572da875a5ed48
Marcello Magno <marcello_magno@hotmail.com>
2022-07-15 22:29:51 -0300

    autent 1.0

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,23 @@
 class SessionsController < ApplicationController
   def new
+
   end
 
-  def create    
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      sing_in(user)   
+      redirect_to user_path(user)
+    else
+      flash.now[:danger] = 'Invalid email or password'
+      render 'new'
+    end
+  end
+
+  def destroy
+    sign_out
+    flash[:warning] = 'login on'
+    redirect_to login_path
   end
   
-end
+end 
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,19 @@
 module SessionsHelper
+    def sing_in
+        session[:user_id] = user.id        
+    end
+    
+    def sing_out
+        session.delete(user.id)        
+    end
+    
+    def current_user
+        @current_user ||= User.find_by(id: session[:user.id])
+    end
+
+    def user_singned_in?
+        !current_user().nil?        
+    end
+    
+    
 end
